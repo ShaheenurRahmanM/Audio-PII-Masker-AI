913b6b0357f1560ab9dbb7fa20c9e131966c199d
ShaheenurRahmanM <shaheenur2005@gamil.com>
2025-05-29 13:03:18 +0530

    Initial Commit Audio PII Masker AI

---
+++ transcriber.py
@@ -0,0 +1,11 @@
+import whisper
+import spacy
+
+
+# Load Whisper model and spaCy model once
+model = whisper.load_model("base")
+nlp = spacy.load("en_core_web_sm")
+
+def transcribe_audio(audio_path):
+    result = model.transcribe(audio_path)
+    return result["text"]
